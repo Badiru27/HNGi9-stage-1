b4ed5bafc8d90c19d4b6ed2bdf7615170d981936
Badiru <badirusulaimon27@gmail.com>
2022-10-27 20:49:28 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+const https = require('http');
+
+const server = https.createServer((req, res)=>{
+    if(req.method !== 'GET') return;
+    res.writeHead(200, {"Content-Type" : "application/json"});
+    res.write(JSON.stringify({ "slackUsername": "Badiru", "backend": true, "age": 23, "bio": "A passionate software. Here to learn and relearn" }));
+    res.end();
+
+});
+
+const PORT = 3000;
+server.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+    
+})
